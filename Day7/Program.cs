67837cac9c8c8d65061e381c32142758987f4947
Evania <em1419@cs.ship.edu>
2016-12-07 23:04:10 -0500

    Day 7, Part 1 code submission.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Day7
 {
+    /// <summary>
+    /// Day 7: Internet Protocol Version 7
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
             var file = File.ReadAllLines("../../day7input.txt");
             var testfile = File.ReadAllLines("../../testinput.txt");
-
 
             ProcessInputA(file);
 
@@ -24,6 +22,7 @@ namespace Day7
 
         public static void ProcessInputA(string[] lines)
         {
+            var len = 4;
             var validIps = 0;
             foreach (var line in lines)
             {
@@ -32,16 +31,25 @@ namespace Day7
 
                 var splits = SplitLine(line);
 
+                // check for palindromes in the words not in brackets
                 foreach (var s in splits.Words)
                 {
                     for (var i = 0; i < s.Length; i++)
                     {
-                        var result = IsPalindrome(new string(s.Skip(i).ToArray()));
+                        var start = i;
+                        var end = (i + len) - 1;
 
-                        if (result)
+                        if (end < s.Length-1)
                         {
-                            wordsPalindrome = true;
-                            break;
+                            var newWord = s.Substring(start, len);
+
+                            var result = IsPalindrome(newWord);
+
+                            if (result)
+                            {
+                                wordsPalindrome = true;
+                                break;
+                            }
                         }
                     }
 
@@ -49,16 +57,25 @@ namespace Day7
                         break;
                 }
 
+                // check for any palindromes in the words between brackets
                 foreach (var b in splits.BracketWords)
                 {
                     for (var i = 0; i < b.Length; i++)
                     {
-                        var result = IsPalindrome(new string(b.Skip(i).ToArray()));
+                        var start = i;
+                        var end = (i + len) - 1;
 
-                        if (result)
+                        if (end < b.Length - 1)
                         {
-                            bracketsPalindrome = true;
-                            break;
+                            var newWord = b.Substring(i, len);
+
+                            var result = IsPalindrome(newWord);
+
+                            if (result)
+                            {
+                                bracketsPalindrome = true;
+                                break;
+                            }
                         }
                     }
 
@@ -66,55 +83,22 @@ namespace Day7
                         break;
                 }
 
+                // if there is at least one palindrome in a word outside of brackets,
+                // and no palindromes in words within brackets, then the IP is valid
                 if (wordsPalindrome && bracketsPalindrome == false)
                 {
                     validIps++;
                 }
-
-                /*var splits = line.Split(new string[] {"[", "]"}, StringSplitOptions.None);
-                var leftPalindrome = false;
-                var middlePalindrome = false;
-                var rightPalindrome = false;
-
-                for (var i = 0; i < splits[0].Length; i++) { 
-                    var palindromeFound = IsPalindrome(new string(splits[0].Skip(i).ToArray()));
-
-                    if (palindromeFound)
-                    {
-                        leftPalindrome = true;
-                        break;
-                    }
-                }
-
-                for (var i = 0; i < splits[1].Length; i++)
-                {
-                    var palindromeFound = IsPalindrome(new string(splits[1].Skip(i).ToArray()));
-
-                    if (palindromeFound)
-                    {
-                        middlePalindrome = true;
-                        break;
-                    }
-                }
-
-                for (var i = 0; i < splits[2].Length; i++)
-                {
-                    var palindromeFound = IsPalindrome(new string(splits[2].Skip(i).ToArray()));
-
-                    if (palindromeFound)
-                    {
-                        rightPalindrome = true;
-                        break;
-                    }
-                }
-
-                if ((leftPalindrome == true || rightPalindrome == true) && middlePalindrome == false)
-                    validIps++;*/
             }
 
-            Console.WriteLine("Valid IPs = " + validIps);
+            Console.WriteLine("Valid ABBA IPs = " + validIps);
         }
 
+        /// <summary>
+        /// Checks to see if a word is a palindrome
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
         public static bool IsPalindrome(string word)
         {
             var min = 0;
@@ -135,9 +119,13 @@ namespace Day7
                 min++;
                 max--;
             }
-
         }
 
+        /// <summary>
+        /// Splits a string into words within brackets and words not within brackets
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static Splits SplitLine(string line)
         {
             var splits = new List<string>();
@@ -151,8 +139,6 @@ namespace Day7
             {
                 bracketSplits.Add(m.Groups[1].Value);
             }
-
-            Console.WriteLine("==================================================================");
 
             var patternOutsideBrackets = @"\](.*?)\[";
 
@@ -168,33 +154,6 @@ namespace Day7
             var endingString = line.Substring(line.LastIndexOf(']') + 1);
             splits.Add(endingString);
 
-            /*while (!string.IsNullOrEmpty(test) || !string.IsNullOrEmpty(test2))
-            {
-                var t = new string(line.Skip(nextLeftIndex).ToArray());
-                var group1 = Regex.Match(t, @"\[([^]]*)\]").Groups[1];
-                test = group1.Value; // finds words in [...]
-
-                if (!string.IsNullOrEmpty(test))
-                {
-                    bracketSplits.Add(test);
-                    nextLeftIndex = group1.Index + 1;
-                }
-
-                var u = new string(line.Skip(nextRightIndex).ToArray());
-                var group2 = Regex.Match(line, @"\]([^[]*)\[").Groups[1];
-                test2 = group2.Value; // finds words in ]...[
-                if (!string.IsNullOrEmpty(test2))
-                {
-                    splits.Add(test2);
-                    nextRightIndex = group2.Index + 1;
-                }
-            }
-            var beginningString = line.Substring(0, line.IndexOf('['));
-            splits.Add(beginningString);
-            var endingString = line.Substring(line.LastIndexOf(']') + 1);
-            splits.Add(endingString);*/
-
-
             return new Splits
             {
                 Words = splits,
@@ -208,4 +167,5 @@ namespace Day7
             public List<string> BracketWords { get; set; }
         }
     }
+
 }
