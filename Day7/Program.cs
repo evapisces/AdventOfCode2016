b4de6b4a458e7244f9a5b4db7a833173606a8552
Evania <em1419@cs.ship.edu>
2016-12-11 23:51:29 -0500

    Day 7, Part 2 code. Day 9 project skeleton code.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Day7
@@ -16,6 +18,8 @@ namespace Day7
             var testfile = File.ReadAllLines("../../testinput.txt");
 
             ProcessInputA(file);
+
+            ProcessInputB(file);
 
             Console.Read();
         }
@@ -88,6 +92,96 @@ namespace Day7
                 if (wordsPalindrome && bracketsPalindrome == false)
                 {
                     validIps++;
+                }
+            }
+
+            Console.WriteLine("Valid ABBA IPs = " + validIps);
+        }
+
+        public static void ProcessInputB(string[] lines)
+        {
+            var len = 3;
+            var validIps = 0;
+            
+            foreach (var line in lines)
+            {
+                var ipFound = false;
+                var wordsPalindrome = false;
+                var bracketsPalindrome = false;
+                
+                var splits = SplitLine(line);
+                var palindromeWords = new List<string>();
+                // check for palindromes in the words not in brackets
+                foreach (var s in splits.Words)
+                {
+                    if (s.Length > len)
+                    {
+                        for (var i = 0; i < s.Length; i++)
+                        {
+
+                            var start = i;
+                            var end = (i + len) - 1;
+
+                            if (end < s.Length)
+                            {
+                                var newWord = s.Substring(start, len);
+
+                                var result = IsPalindrome(newWord);
+
+                                if (result && newWord.Distinct().Count() > 1)
+                                {
+                                    
+                                    var l1 = newWord[0];
+                                    var l2 = newWord[1];
+                                    var sb = new StringBuilder();
+                                    sb.Append(l2).Append(l1).Append(l2);
+                                    var searchWord = sb.ToString();
+                                    
+                                    foreach (var bWord in splits.BracketWords)
+                                    {
+                                        if (bWord.Contains(searchWord))
+                                        {
+                                            Console.WriteLine("Word = " + s + ",    " + newWord);
+                                            Console.WriteLine("Search word = " + searchWord);
+                                            Console.WriteLine("Bracketed word = " + bWord);
+                                            validIps++;
+                                            ipFound = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            if (ipFound)
+                                break;
+                        }
+                    }
+                    else
+                     {
+                        var result = IsPalindrome(s);
+
+                        if (result && s.Distinct().Count() > 1)
+                        {
+                            Console.WriteLine("Word = " + s + ",    " + s);
+                            var l1 = s[0];
+                            var l2 = s[1];
+                            var sb = new StringBuilder();
+                            sb.Append(l2).Append(l1).Append(l2);
+                            var searchWord = sb.ToString();
+                            Console.WriteLine("Search word = " + searchWord);
+                            foreach (var bWord in splits.BracketWords)
+                            {
+                                if (bWord.Contains(searchWord))
+                                {
+                                    Console.WriteLine("Bracketed word = " + bWord);
+                                    validIps++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (ipFound)
+                        break;
                 }
             }
 
---
+++ Day9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("../../testinput.txt");
+        }
+    }
+}
