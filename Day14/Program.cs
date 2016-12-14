78d6025eebdbdae967e27407c1ee9dfa39dc62f9
Evania <em1419@cs.ship.edu>
2016-12-14 16:45:59 -0500

    Day 14, Part 1 (shelving)

---
+++ Day14/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var salt = "zpqevtbw";
+            var testSalt = "abc";
+
+            ProcessInputA(salt);
+
+
+            Console.Read();
+        }
+
+        public static void ProcessInputA(string salt)
+        {
+            var potentialKeys = new Dictionary<int, string>();
+            var keys = new Dictionary<int, string>();
+            var i = 0;
+            var index = 1;
+
+            while (keys.Count < 64)
+            {
+                var hash = CalculateMdD5Hash(salt + i);
+                var matches = Regex.Matches(hash, @"(\w)\1{2,}");
+
+                if (matches.Count > 0)
+                {
+                    var match = matches[0].Value;
+                    var ch = match[0];
+                    for (var j = i+1; j < 1000 + i; j++)
+                    {
+                        var newHash = CalculateMdD5Hash(salt + j);
+
+                        var consecutiveMatches = Regex.Matches(newHash, @"(" + ch + @")\1{4,}");
+
+                        if (consecutiveMatches.Count > 0)
+                        {
+                            Console.WriteLine("(" + index + ")  Key: " + i + ", " + consecutiveMatches[0].Value);
+                            Console.WriteLine("Hash = " + hash);
+                            Console.WriteLine("NewHash = " + newHash);
+                            keys.Add(i, consecutiveMatches[0].Value);
+                            index++;
+                        }
+                    }
+                }
+
+                i++;
+            }
+
+            var allKeys = keys.OrderBy(k => k.Key);
+            Console.WriteLine("64th key at index " + allKeys.Last().Key);
+        }
+
+        private static string CalculateMdD5Hash(string input)
+        {
+            var md5 = System.Security.Cryptography.MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var hash = md5.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
