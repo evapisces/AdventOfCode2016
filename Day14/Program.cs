02e785550d0888fbcaa94cd618e61e2954f2a929
Evania <em1419@cs.ship.edu>
2016-12-15 16:30:25 -0500

    Day 14, part 2 (shelving)

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -15,8 +15,8 @@ namespace Day14
             var salt = "zpqevtbw";
             var testSalt = "abc";
 
-            ProcessInputA(salt);
-
+            //ProcessInputA(salt);
+            ProcessInputB(testSalt);
 
             Console.Read();
         }
@@ -62,6 +62,46 @@ namespace Day14
             Console.WriteLine("64th key at index " + allKeys.Last().Key);
         }
 
+        public static void ProcessInputB(string salt)
+        {
+            var keys = new Dictionary<int, string>();
+            var i = 0;
+            var index = 1;
+
+            while (keys.Count < 65)
+            {
+                var hash = StretchHash(salt + i);
+                var matches = Regex.Matches(hash, @"(\w)\1{2,}");
+
+                if (matches.Count > 0)
+                {
+                    var match = matches[0].Value;
+                    var ch = match[0];
+                    for (var j = i + 1; j <= 1000 + i; j++)
+                    {
+                        var newHash = CalculateMdD5Hash(salt + j);
+
+                        var consecutiveMatches = Regex.Matches(newHash, @"(" + ch + @")\1{4,}");
+
+                        if (consecutiveMatches.Count > 0)
+                        {
+                            Console.WriteLine("(" + index + ")  Key: " + i + ", " + consecutiveMatches[0].Value);
+                            Console.WriteLine("Hash = " + hash);
+                            Console.WriteLine("NewHash = " + newHash);
+                            keys.Add(i, consecutiveMatches[0].Value);
+                            index++;
+                            break;
+                        }
+                    }
+                }
+
+                i++;
+            }
+
+            var allKeys = keys.OrderBy(k => k.Key);
+            Console.WriteLine("64th key at index " + allKeys.Last().Key);
+        }
+
         private static string CalculateMdD5Hash(string input)
         {
             var md5 = System.Security.Cryptography.MD5.Create();
@@ -75,6 +115,16 @@ namespace Day14
             }
 
             return sb.ToString();
+        }
+
+        private static string StretchHash(string hash)
+        {
+            for (var i = 0; i < 2017; i++)
+            {
+                hash = CalculateMdD5Hash(hash);
+            }
+
+            return hash;
         }
     }
 }
